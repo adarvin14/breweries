f5174c6a6635e092a714e7dfe6bcce6c770dc1d0
Alex Darvin <amdarvin14@gmail.com>
2020-09-29 16:38:17 +0000

    add initialize method

--- lib/beer.rb
+++ lib/beer.rb
@@ -1,7 +1,20 @@
 class Beer
   
-  attr_accessor :id, :name, :tagline, :first_brewed, :description, :abv, :ingredients
+  attr_accessor :id, :name, :tagline, :first_brewed, :description, :abv, :ingredients, :image_url
   
-
+  @@all = []
+  
+  def initialize
+    @name = name
+    @image_url = image_url
+    @id = id
+    @tagline = tagline
+    @@all << self
+  end
+  
+  def self.all
+    @@all
+  end
+  
   
 end
