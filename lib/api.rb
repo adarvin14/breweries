76c885bbab1ce0dd395184e4d65fa2a2f31cca8d
Alex Darvin <amdarvin14@gmail.com>
2020-09-30 16:13:11 +0000

    add to API methods

--- lib/api.rb
+++ lib/api.rb
@@ -4,12 +4,16 @@ class API
     url = "https://api.punkapi.com/v2/beers"
     uri = URI(url)
     response = Net::HTTP.get(uri)
+    
+    beers = JSON.parse(response)
   end
   
   def self.get_beer(id)
     url = "https://api.punkapi.com/v2/beers/#{id}"
     uri = URI(url)
     response = Net::HTTP.get(uri)
+    
+    beer = JSON.parse(response)
   end
   
 end
--- lib/cli.rb
+++ lib/cli.rb
@@ -16,18 +16,23 @@ class CLI
     Beer.find_by_id(id.to_i-1)
       while input != "exit" do
         if input > 0 && input <= 25
-          #print beer
-          
-        elsif input 
+          API.get_beer
+        elsif input != i && input <= 0 && input > 25
     puts
     puts ""
     puts "----------------------------------------------------------------------------------------------------------"
     puts "If you would like to see the list of beers again, enter 'beers' or enter another number to view a different beer."
+    input = gets.strip.downcase
           if input = "beers"
             print_beers
+          elsif input > 0 && input <= 25
+            print_beer
           else
-          end
     puts ""
+    puts "--------------------------------------------------------"
+    puts "We can't find what you're looking for. Please try again."
+    puts ""
+          end
         else
     puts ""
     puts "------------------------------------------------"
@@ -41,6 +46,4 @@ class CLI
     puts ""
     end
 
-  
-  
 end
