a641d3f45e8229fbc20cacdd0a49438864acdf02
Alex Darvin <amdarvin14@gmail.com>
2020-10-01 19:53:59 +0000

    add prompt method and comment out print_beer method

--- lib/cli.rb
+++ lib/cli.rb
@@ -12,20 +12,20 @@ class CLI
     puts "Enter a number to learn more about a beer you would like to brew or 'exit' to exit."
     puts ""
     
-    input = gets.strip
-      while input != "exit" do
+    while input != "exit" do
         if input.to_i > 0 && input.to_i <= 25
-          print_beer
+          beer = Beer.all[input.to_i]
         elsif input != input.to_i || input.to_i <= 0 || input.to_i > 25
     puts
     puts ""
     puts "----------------------------------------------------------------------------------------------------------"
     puts "If you would like to see the list of beers again, enter 'beers' or enter another number to view a different beer."
+    puts ""
     input = gets.strip.downcase
           if input == "beers"
             print_beers
           elsif input.to_i > 0 && input.to_i <= 25
-            print_beers
+            #print_beer
           else
     puts ""
     puts "--------------------------------------------------------"
@@ -33,10 +33,7 @@ class CLI
     puts ""
           end
         else
-    puts ""
-    puts "------------------------------------------------"
-    puts "Enter another number to view a different recipe."
-    puts ""
+          prompt
         end
     end
      puts ""
@@ -46,14 +43,21 @@ class CLI
     puts ""
   end
   
+    def prompt
+      puts ""
+      puts "------------------------------------------------"
+      puts "Enter another number to view a different recipe, or enter 'exit' to exit."
+      puts ""
+    end
+  
     def print_beers
       Beer.all.each_with_index do |beer, i|
         puts "#{i}. #{beer.name}"
       end
     end
     
-    def print_beer
-      Beer.find_by_id
+    def print_beer(beer)
+      #Beer.find_by_id  show recipe for selected beer
     end
 
 end
