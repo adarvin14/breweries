99896b61a83f9ea17f9693eda840144059c6e44f
Alex Darvin <amdarvin14@gmail.com>
2021-02-02 19:04:05 +0000

    Automatically backed up by Learn

--- config/environment.rb
+++
@@ -1,8 +0,0 @@
-require 'pry'
-require 'httparty'
-require 'json'
-
-require_relative "../lib/api"
-require_relative "../lib/beer"
-require_relative "../lib/cli"
-require_relative "../lib/ingredient"
